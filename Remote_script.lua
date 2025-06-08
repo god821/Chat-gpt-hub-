b5cf7265e41de2454e32ad8056b78d531f16099a
god821 <mateusjogos001@gmail.com>
2025-06-07 21:45:56 -0300

    Add remote_script.lua for GPT Hub executor

---
+++ Remote_script.lua
@@ -0,0 +1,7 @@
+return {
+    notify = "👋 GOD HUB Online!",
+    execute = [[
+        print("Olá do script remoto!")
+        -- Aqui você pode colocar qualquer código Lua que quiser executar no executor
+    ]]
+}
